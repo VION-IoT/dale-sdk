f1f3f2b059d5dcbb3160dc7e88f9671b9ee43b66
jonasbertsch <jonas.bertsch@gmail.com>
2026-09-14 14:58:48 +0200

    Register the HTTP example for upload, packing and version bumps

--- scripts/pack-examples.ps1
+++ scripts/pack-examples.ps1
@@ -14,6 +14,7 @@ $projects = @(
     "examples\Vion.Examples.Energy\Vion.Examples.Energy\Vion.Examples.Energy.csproj",
     "examples\Vion.Examples.ModbusRtu\Vion.Examples.ModbusRtu\Vion.Examples.ModbusRtu.csproj",
     "examples\Vion.Examples.ModbusTcp\Vion.Examples.ModbusTcp\Vion.Examples.ModbusTcp.csproj",
+    "examples\Vion.Examples.Http\Vion.Examples.Http\Vion.Examples.Http.csproj",
     "examples\Vion.Examples.RichTypes\Vion.Examples.RichTypes\Vion.Examples.RichTypes.csproj",
     "examples\Vion.Examples.Emission\Vion.Examples.Emission\Vion.Examples.Emission.csproj",
     "examples\Vion.Examples.Gating\Vion.Examples.Gating\Vion.Examples.Gating.csproj"
